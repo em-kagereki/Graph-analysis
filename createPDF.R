c836aa3293d37ebe6dcc0424974a429858355d80
em-kagereki <emkagereki@gmail.com>
2022-02-25 09:26:00 -0400

    Add the PDF

--- createPDF.R
+++ createPDF.R
@@ -1,31 +1,16 @@
 library(xaringanBuilder)
-build_pdf("C:/Users/Admin/Documents/KM - Winter 2022/tutorial/B00867154/Report.html")
-
-browser="google-chrome-stable"
-pagedown::chrome_print("C:/Users/Admin/Documents/KM - Winter 2022/tutorial/B00867154/Report.html",output="Report.pdf")
-build_pdf("C:/Users/Admin/Documents/KM - Winter 2022/tutorial/B00867154/Report.html", complex_slides = TRUE)
-
-build_pdf("C:/Users/Admin/Documents/KM - Winter 2022/tutorial/B00867154/Report.Rmd", complex_slides = TRUE)
 
 
-library(chromote)
 
-build_pdf(input = "C:/Users/Admin/Documents/KM - Winter 2022/tutorial/B00867154/Report.html",
-          output_file = "C:/Users/Admin/Documents/KM - Winter 2022/tutorial/B00867154/Report.pdf",
+build_pdf(input = "Data/Report.Rmd",
+          output_file = "Data/Report.pdf",
           complex_slides = TRUE,
           partial_slides = TRUE)
 
 
-build_pdf(input = "C:/Users/Admin/Documents/KM - Winter 2022/tutorial/B00867154/Report.Rmd",
-          output_file = "C:/Users/Admin/Documents/KM - Winter 2022/tutorial/B00867154/Report.pdf",
+build_pptx(input = "C:/Users/Admin/Documents/KM - Winter 2022/tutorial/B00867154/Report.Rmd",
+          output_file = "C:/Users/Admin/Documents/KM - Winter 2022/tutorial/B00867154/Report.pptx",
           complex_slides = TRUE,
           partial_slides = TRUE)
 
-library(xaringanBuilder)
-setwd("C:/Users/Admin/Documents/KM - Winter 2022/tutorial/B00867154/")
-#build_pdf("Report.Rmd")
-build_pdf("Report.html")
-
-
-pagedown::chrome_print("test.html",output="test.pdf")
 
